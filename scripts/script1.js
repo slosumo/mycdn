61b8663aa3217ee1efc82157512c37b6968126bc
AndrazVene <andraz.vene@gmail.com>
2023-09-02 11:18:03 +0200

    - dev first commit

--- scripts/script1.js
+++ scripts/script1.js
@@ -1,2 +1,2 @@
 const SCRIPT_VERSION = "1.0.0.0";
-console.log("script 1 loaded, version: " + SCRIPT_VERSION);
+console.log("dev script 1 loaded, version: " + SCRIPT_VERSION);
--- scripts/script2.js
+++ scripts/script2.js
@@ -1,2 +1,2 @@
 const SCRIPT_VERSION = "1.0.0.0";
-console.log("script 2 loaded, version: " + SCRIPT_VERSION);
+console.log("dev script 2 loaded, version: " + SCRIPT_VERSION);
